fd5edacbf5a7c0bde3d9756de93f6d198bc494e8
EdgeCaser <EdgeCaser@users.noreply.github.com>
2022-07-29 20:16:58 -0700

    Formatting and QOL tweaks.

--- Snapshotsurfer.py
+++ Snapshotsurfer.py
@@ -9,7 +9,7 @@ import pandas as pd
 import duckdb as db
 import seaborn as sns
 import matplotlib.pyplot as plt
-import streamlit as st
+#import streamlit as st
 
 st.set_page_config(layout="wide")
 
